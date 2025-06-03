ece2b1500b293ad178a42f8b44170a33c4222ab7
PlutoA713N <mallachandu99@gmail.com>
2025-06-04 00:44:13 +0530

    perf: use yaml for api docs

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,9 +1,14 @@
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
 import { swaggerOptions } from './swagger.config';
+import YAML from "yamljs"
+import path from "node:path";
 import { Express } from 'express';
 
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
+// const swaggerSpec = swaggerJSDoc(swaggerOptions);
+// const swaggerSpec = YAML.parse(JSON.stringify(swaggerOptions));
+const swaggerSpec = YAML.load(path.resolve(__dirname, 'openapi.yaml'))
+
 
 export const setupSwaggerDocs = (app: Express) => {
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));

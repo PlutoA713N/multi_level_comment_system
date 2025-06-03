1efbb3877076583e85b132042b72c795dd2b7905
PlutoA713N <mallachandu99@gmail.com>
2025-06-04 01:07:21 +0530

    feat: add success constants

---
+++ src/constants/success.constants.ts
@@ -0,0 +1,17 @@
+export const USER_MESSAGES = {
+    LOGIN_SUCCESS: 'User logged in successfully',
+    REGISTER_SUCCESS: 'User registered successfully',
+};
+
+export const USER_EVENTS = {
+    LOGIN_SUCCESS: 'USER_LOGGED_IN',
+    LOGIN_FAILED: 'LOGIN_FAILED',
+};
+
+export const MONGO_EVENTS = {
+    MONGO_OK:'MONGO_OK'
+}
+
+export const MONGO_MESSAGES = {
+    MONGO_CONNECTION_SUCCESS: 'MongoDB connected',
+}

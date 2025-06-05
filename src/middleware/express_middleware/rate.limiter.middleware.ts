2458818e6140d26db87cf22674d04fcea1ae190f
PlutoA713N <mallachandu99@gmail.com>
2025-06-05 05:47:37 +0530

    refactor: use dry principle and make handler reusable

--- src/middleware/express_middleware/rate.limiter.middleware.ts
+++ src/middleware/express_middleware/rate.limiter.middleware.ts
@@ -1,26 +1,27 @@
 import rateLimit from 'express-rate-limit';
 import { Request, Response } from 'express';
-import { createApiError } from '../../factory/create.api.error';
+import {createApiError} from '../../factory/create.api.error';
 import logger from "../../logger";
 import {ERROR_CODES} from "../../constants/error.constants";
 import HTTP from "http-status-codes";
 import {HTTP_ERROR_DETAILS} from "../../constants/error.details.constants";
+import {getEnv} from "../../config/env";
 
-export const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 50,
-    standardHeaders: true,
-    legacyHeaders: false,
-    handler: (req: Request, res: Response) => {
+const API_RATE_LIMIT_WINDOW_MINUTES = Number(getEnv('API_RATE_LIMIT_WINDOW_MINUTES', false, '15'))
+const API_RATE_LIMIT_REQUESTS = Number(getEnv('API_RATE_LIMIT_REQUESTS', false, '50'))
+const LOGIN_RATE_LIMIT_WINDOW_MINUTES = Number(getEnv('LOGIN_RATE_LIMIT_WINDOW_MINUTES', false, '10'));
+const LOGIN_RATE_LIMIT_REQUESTS = Number(getEnv('LOGIN_RATE_LIMIT_REQUESTS', false, '5'))
 
+function createRateLimitHandler(code: string, detail: string, logPrefix: string )  {
+    return (req: Request, res: Response) => {
         const error = createApiError({
             status: HTTP.TOO_MANY_REQUESTS,
-            code: ERROR_CODES.TOO_MANY_REQUESTS,
-            detail: HTTP_ERROR_DETAILS.TOO_MANY_REQUESTS,
+            code: code,
+            detail: detail,
             instance: req.originalUrl,
         });
 
-        logger.warn(`[RateLimit] ${req.ip} exceeded limit on ${req.originalUrl}`, {
+        logger.warn(`[${logPrefix}] ${req.ip} exceeded limit on ${req.originalUrl}`, {
             ip: req.ip,
             path: req.originalUrl,
             requestId: req.headers['x-request-id'] as string,
@@ -28,31 +29,22 @@ export const apiLimiter = rateLimit({
         });
 
         res.status(error.status).json(error);
-    },
+    }
+}
+
+export const apiLimiter = rateLimit({
+    windowMs: API_RATE_LIMIT_WINDOW_MINUTES * 60 * 1000,
+    limit: API_RATE_LIMIT_REQUESTS,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: createRateLimitHandler(ERROR_CODES.TOO_MANY_REQUESTS,HTTP_ERROR_DETAILS.TOO_MANY_REQUESTS,'RateLimit')
 });
 
 
 export const loginRateLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    limit: 3,
+    windowMs: LOGIN_RATE_LIMIT_WINDOW_MINUTES * 60 * 1000,
+    limit: LOGIN_RATE_LIMIT_REQUESTS,
     standardHeaders: true,
     legacyHeaders: false,
-    handler: (req: Request, res: Response) => {
-
-        const error = createApiError({
-            status: HTTP.TOO_MANY_REQUESTS,
-            code: ERROR_CODES.TOO_MANY_LOGIN_ATTEMPTS,
-            detail: HTTP_ERROR_DETAILS.TOO_MANY_LOGIN_ATTEMPTS,
-            instance: req.originalUrl,
-        });
-
-        logger.warn(`[LoginRateLimit] ${req.ip} blocked from logging in`, {
-            ip: req.ip,
-            path: req.originalUrl,
-            requestId: req.headers['x-request-id'] as string,
-            userAgent: req.headers['user-agent'],
-        });
-
-        res.status(error.status).json(error);
-    },
+    handler: createRateLimitHandler(ERROR_CODES.TOO_MANY_LOGIN_ATTEMPTS, HTTP_ERROR_DETAILS.TOO_MANY_LOGIN_ATTEMPTS, 'LoginRateLimit')
 });

3b57b225efc38ad45edcc78fe1732db772763e3e
PlutoA713N <mallachandu99@gmail.com>
2025-06-05 05:48:19 +0530

    refactor: update usermodel name

--- src/models/user/user.model.ts
+++ src/models/user/user.model.ts
@@ -1,6 +1,7 @@
-import  { Types, model, Schema , Document} from "mongoose"
+import {model, Schema, Types} from "mongoose"
 
-export interface IUserRegistration{
+export interface IUser{
+    _id: Types.ObjectId;
     username: string,
     email: string,
     password: string,
@@ -8,20 +9,10 @@ export interface IUserRegistration{
     updatedAt: Date,
 }
 
-export interface IUser extends IUserRegistration {
-    _id: Types.ObjectId;
-}
-
-export interface IUserLogin extends Document {
-    username?: string;
-    email?: string;
-    password: string;
-}
-
-const userRegistrationSchema = new Schema<IUserRegistration>({
+const userSchema = new Schema<IUser>({
     username: {type: String, required: true, unique: true, trim: true, minlength:3, maxlength:20},
     email: {type: String, required: true, unique: true, trim: true,  match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/},
     password: {type: String, required: true, minlength: 8, match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/ },
 }, {timestamps: true})
 
-export const UserRegistrationModel = model('UserRegistration', userRegistrationSchema)
+export const UserModel = model('User', userSchema)

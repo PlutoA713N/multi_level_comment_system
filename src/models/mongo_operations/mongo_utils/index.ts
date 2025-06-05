de4dfce0196db1556d51a2ab59db7cd2071d48f6
PlutoA713N <mallachandu99@gmail.com>
2025-06-05 05:45:31 +0530

    feat: add validate and attach()

--- src/models/mongo_operations/mongo_utils/index.ts
+++ src/models/mongo_operations/mongo_utils/index.ts
@@ -1,12 +1,15 @@
 import {Model} from "mongoose";
 import { AppError } from "./../../../class/app.error.class";
 import HTTP from "http-status-codes";
-import {ERROR_CODES, MONGO_ERROR_CODES} from "../../../constants/error.constants";
-import {UserRegistrationModel} from "../../user/user.registration.model";
+import { MONGO_ERROR_CODES} from "../../../constants/error.constants";
+import {UserModel} from "../../user/user.model";
+import {createApiError} from "../../../factory/create.api.error";
+import logger from "../../../logger";
+import {AttachField, ICustomRequest} from "../../../interfaces/Request_interfaces";
 
 interface CheckResult <T> {
     message: string,
-    user: T | null,
+    doc: T | null,
     isExists: boolean
 }
 
@@ -14,10 +17,11 @@ export async function checkFieldExists<T = any>(model: Model<any>, field: string
     try {
         const existingUser = await model.findOne({ [field]: value })
         if (existingUser) {
-            return { message: `${[field]} already exists`, user: existingUser as T, isExists: true }
+            return { message: `${field} exists`, doc: existingUser as T, isExists: true }
         }
-        return { message: '', user: null, isExists: false }
-    } catch (error) {
+        return { message: '', doc: null, isExists: false }
+    } catch (error: any) {
+        logger.error(`[DB Check Failed] ${field}: ${error.message}`);
         throw new AppError(
             `Failed to check ${field}: ${error instanceof Error ? error.message : String(error)}`,
             HTTP.INTERNAL_SERVER_ERROR,
@@ -29,12 +33,23 @@ export async function checkFieldExists<T = any>(model: Model<any>, field: string
 export const findUserByField = async (form: object) => {
     for (const [field, value] of Object.entries(form)) {
         if (value) {
-            const result = await checkFieldExists(UserRegistrationModel, field, value)
-            if (result.isExists && result.user) {
-                return result.user
+            const result = await checkFieldExists(UserModel, field, value)
+            if (result.isExists && result.doc) {
+                return result.doc
             }
         }
     }
 
     return null
+}
+
+
+export async function validateAndAttach(model: Model<any>,fieldId: string, field: AttachField, req: ICustomRequest) {
+    const result = await checkFieldExists(model,'_id', fieldId);
+    if (!result.isExists) {
+        throw createApiError({detail: `${field} does not exist`, status:HTTP.NOT_FOUND, code:`${field.toUpperCase()}_NOT_FOUND`});
+    }
+
+    (req as any)[field] = result.doc;
+    return req
 }

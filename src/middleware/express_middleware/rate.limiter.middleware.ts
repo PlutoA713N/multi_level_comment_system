05cf726ed4edfb87ceeb2233b792f54c954cebcc
PlutoA713N <mallachandu99@gmail.com>
2025-06-06 21:08:22 +0530

    refactor: make code modular

--- src/middleware/express_middleware/rate.limiter.middleware.ts
+++ src/middleware/express_middleware/rate.limiter.middleware.ts
@@ -1,61 +1,28 @@
-import rateLimit from 'express-rate-limit';
-import { Request, Response } from 'express';
-import {createApiError} from '../../factory/create.api.error';
-import logger from "../../logger";
-import {ERROR_CODES} from "../../constants/error.constants";
-import HTTP from "http-status-codes";
-import {HTTP_ERROR_DETAILS} from "../../constants/error.details.constants";
-import {getEnv} from "../../config/env";
+import { ERROR_CODES } from "../../constants/error.constants";
+import { HTTP_ERROR_DETAILS } from "../../constants/error.details.constants";
+import {createRateLimitHandler, createLimiter} from "../../factory/create.rate.limiter";
+import {LIMITS} from "../../config/rate-limit/rate.limit.config";
 
-const API_RATE_LIMIT_WINDOW_MINUTES = Number(getEnv('API_RATE_LIMIT_WINDOW_MINUTES', false, '15'))
-const API_RATE_LIMIT_REQUESTS = Number(getEnv('API_RATE_LIMIT_REQUESTS', false, '50'))
-const LOGIN_RATE_LIMIT_WINDOW_MINUTES = Number(getEnv('LOGIN_RATE_LIMIT_WINDOW_MINUTES', false, '10'));
-const LOGIN_RATE_LIMIT_REQUESTS = Number(getEnv('LOGIN_RATE_LIMIT_REQUESTS', false, '5'))
-const POST_COMMENT_RATE_LIMIT_WINDOW_MINUTES = Number(getEnv('POST_COMMENT_RATE_LIMIT_WINDOW_MINUTES', false, '10'));
-const POST_COMMENT_RATE_LIMTI_REQUESTS = Number(getEnv('POST_COMMENT_RATE_LIMTI_REQUESTS', false, '5'))
+export const apiLimiter = createLimiter(
+    LIMITS.API.windowMinutes,
+    LIMITS.API.maxRequests,
+    createRateLimitHandler(ERROR_CODES.TOO_MANY_REQUESTS, HTTP_ERROR_DETAILS.TOO_MANY_REQUESTS, 'API')
+);
 
-function createRateLimitHandler(code: string, detail: string, logPrefix: string )  {
-    return (req: Request, res: Response) => {
-        const error = createApiError({
-            status: HTTP.TOO_MANY_REQUESTS,
-            code: code,
-            detail: detail,
-            instance: req.originalUrl,
-        });
+export const loginRateLimiter = createLimiter(
+    LIMITS.LOGIN.windowMinutes,
+    LIMITS.LOGIN.maxRequests,
+    createRateLimitHandler(ERROR_CODES.TOO_MANY_LOGIN_ATTEMPTS, HTTP_ERROR_DETAILS.TOO_MANY_LOGIN_ATTEMPTS, 'Login')
+);
 
-        logger.warn(`[${logPrefix}] ${req.ip} exceeded limit on ${req.originalUrl}`, {
-            ip: req.ip,
-            path: req.originalUrl,
-            requestId: req.headers['x-request-id'] as string,
-            userAgent: req.headers['user-agent'],
-        });
+export const postCommentRateLimiter = createLimiter(
+    LIMITS.COMMENT.windowMinutes,
+    LIMITS.COMMENT.maxRequests,
+    createRateLimitHandler(ERROR_CODES.TOO_MANY_REQUESTS, HTTP_ERROR_DETAILS.TOO_MANY_COMMENT_ATTEMPTS, 'Comment')
+);
 
-        res.status(error.status).json(error);
-    }
-}
-
-export const apiLimiter = rateLimit({
-    windowMs: API_RATE_LIMIT_WINDOW_MINUTES * 60 * 1000,
-    limit: API_RATE_LIMIT_REQUESTS,
-    standardHeaders: true,
-    legacyHeaders: false,
-    handler: createRateLimitHandler(ERROR_CODES.TOO_MANY_REQUESTS,HTTP_ERROR_DETAILS.TOO_MANY_REQUESTS,'RateLimit')
-});
-
-
-export const loginRateLimiter = rateLimit({
-    windowMs: LOGIN_RATE_LIMIT_WINDOW_MINUTES * 60 * 1000,
-    limit: LOGIN_RATE_LIMIT_REQUESTS,
-    standardHeaders: true,
-    legacyHeaders: false,
-    handler: createRateLimitHandler(ERROR_CODES.TOO_MANY_LOGIN_ATTEMPTS, HTTP_ERROR_DETAILS.TOO_MANY_LOGIN_ATTEMPTS, 'LoginRateLimit')
-})
-
-
-export const postCommentRateLimiter = rateLimit({
-    windowMs: POST_COMMENT_RATE_LIMIT_WINDOW_MINUTES * 60 * 1000,
-    limit: POST_COMMENT_RATE_LIMTI_REQUESTS,
-    standardHeaders: true,
-    legacyHeaders: false,
-    handler: createRateLimitHandler(ERROR_CODES.TOO_MANY_REQUESTS, HTTP_ERROR_DETAILS.TOO_MANY_COMMENT_ATTEMPTS, 'CommentRateLimit')
-});
+export const replyCommentRateLimiter = createLimiter(
+    LIMITS.REPLY.windowMinutes,
+    LIMITS.REPLY.maxRequests,
+    createRateLimitHandler(ERROR_CODES.TOO_MANY_REQUESTS, HTTP_ERROR_DETAILS.TOO_MANY_REPLY_ATTEMPTS, 'Reply')
+);

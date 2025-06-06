68b5383c1cf8dee6575e5f7b36892e7b3b972c77
PlutoA713N <mallachandu99@gmail.com>
2025-06-06 21:07:34 +0530

    perf: add auto increment counter to postId

--- src/models/post/post.model.ts
+++ src/models/post/post.model.ts
@@ -1,7 +1,12 @@
-import {Schema, model, Types} from "mongoose";
+import {Schema, model, Types, connection} from "mongoose";
+import  mongooseSequence from "mongoose-sequence";
+
+const AutoIncrementFactory = mongooseSequence(connection)
 
 export interface IPost extends Document {
     _id: Types.ObjectId;
+    __v: number;
+    postId: number;
     userId: Types.ObjectId;
     title: string;
     content: string;
@@ -10,10 +15,12 @@ export interface IPost extends Document {
 }
 
 export const postSchema = new Schema<IPost>({
+    postId: {type: Number, unique: true},
     userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
     title: {type: String, required: true, trim: true, minLength: 3, maxLength: 50},
     content: {type: String, required: true, trim: true, minLength: 1, maxLength: 150},
 }, {timestamps: true});
 
+postSchema.plugin(AutoIncrementFactory, {inc_field: 'postId'});
 
 export const PostModel = model("Post", postSchema);

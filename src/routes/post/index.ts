0f5fa8f16bcbcc8c466c969b4760e1fcaab97eaa
PlutoA713N <mallachandu99@gmail.com>
2025-06-06 21:04:55 +0530

    feat: add routes

--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -1,14 +1,32 @@
 import {Router} from "express";
-import {validatePostCommentRules, validatePostRules} from "../../middleware/post_middleware";
+import {
+    validateGetPostsRules,
+    validatePostCommentRules,
+    validatePostRules,
+    validateReplyCommentRules
+} from "../../middleware/post_middleware";
 import {validateResult} from "../../middleware/user_middleware/validate.middleware";
 import {authenticationHandler} from "../../middleware/authentication_middleware/authentication";
-import {createPostController} from "../../controller/post/post.create.controller";
+import {createPostController} from "../../controller/post/create.post.controller";
 import {postCommentController} from "../../controller/post/post.comment.controller";
-import {postCommentRateLimiter} from "../../middleware/express_middleware/rate.limiter.middleware";
+import {
+    postCommentRateLimiter,
+    replyCommentRateLimiter
+} from "../../middleware/express_middleware/rate.limiter.middleware";
+import {
+    lookupPostDocument,
+    lookupPostReplyCommentDocuments
+} from "../../middleware/search_document_middleware";
+import {replyCommentController} from "../../controller/post/reply.comment.controller";
+
 const router = Router();
 
 router.post("/", authenticationHandler, ...validatePostRules, validateResult, createPostController);
 
-router.post("/:postId/comments", postCommentRateLimiter, authenticationHandler, ...validatePostCommentRules, validateResult, postCommentController );
+router.post("/:postId/comments", postCommentRateLimiter, authenticationHandler, ...validatePostCommentRules, validateResult, lookupPostDocument, postCommentController );
+
+router.post("/:postId/comments/:commentId/reply", replyCommentRateLimiter, authenticationHandler, ...validateReplyCommentRules, validateResult, lookupPostReplyCommentDocuments, replyCommentController );
+
+router.get("/:postId/comments", authenticationHandler, ...validateGetPostsRules, validateResult, lookupPostDocument, )
 
 export default router;

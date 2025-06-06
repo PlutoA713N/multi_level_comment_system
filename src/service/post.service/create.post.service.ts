0c1520f9fdf7ac47f6fd202a1ae7a3157c2539c5
PlutoA713N <mallachandu99@gmail.com>
2025-06-06 21:02:44 +0530

    feat: implement post service

--- src/service/post.service/create.post.service.ts
+++ src/service/post.service/create.post.service.ts
@@ -1,13 +1,13 @@
 import { PostModel } from "../../models/post/post.model";
 import {Types} from "mongoose";
 
-export const postCreateService = async (title: string, content: string, userId: Types.ObjectId) => {
+export const createPostService = async (title: string, content: string, userId: Types.ObjectId) => {
     const post = new PostModel({title, content, userId})
     const savedDocument = await post.save();
 
     const postObject = savedDocument.toObject();
 
-    const {_id, ...postFields} = postObject;
+    const {_id, __v, ...postFields} = postObject;
 
     return {post: postFields};
 }

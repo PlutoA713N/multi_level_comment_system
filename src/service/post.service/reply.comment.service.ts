bf5901be9a8425f81249361bdb1551ea8945d5fc
PlutoA713N <mallachandu99@gmail.com>
2025-06-06 21:13:19 +0530

    feat: implement rate-limit service

---
+++ src/service/post.service/reply.comment.service.ts
@@ -0,0 +1,18 @@
+import {Types} from "mongoose";
+import {CommentModel} from "../../models/post/comment.model";
+
+export async function replyCommentService(data: { commentId: number; postId: number; text: string; userId: Types.ObjectId }) {
+
+    const commentInstance = new CommentModel({
+        text: data.text,
+        userId: data.userId,
+        postId: data.postId,
+        parentCommentId: data.commentId,
+    })
+
+    const commentDocument = await commentInstance.save()
+    const commentObject = commentDocument.toObject();
+
+    const {_id, __v, userId, ...comment} = commentObject
+    return {comment}
+}

4ec6f2f06b3f52c8ad670ec1d663ad4382188278
PlutoA713N <mallachandu99@gmail.com>
2025-06-06 21:03:43 +0530

    feat: make req interface generic

--- src/interfaces/Request_interfaces/index.ts
+++ src/interfaces/Request_interfaces/index.ts
@@ -2,10 +2,22 @@ import {IUser} from "../../models/user/user.model";
 import {IPost} from "../../models/post/post.model";
 import {Request} from "express";
 
-export interface ICustomRequest extends Request  {
+export interface ICustomRequest<
+    P = Record<string, any>,
+    B = Record<string, any>,
+    Q = Record<string, any>,
+> extends Request<P,any, B, Q> {
     user?: IUser
     post?: IPost
 }
 
 export type AttachField = Extract<keyof ICustomRequest, string>;
 
+export interface IReplyCommentParams {
+    commentId: string;
+    postId: string;
+}
+
+export interface IReplyCommentBody {
+    text: string;
+}

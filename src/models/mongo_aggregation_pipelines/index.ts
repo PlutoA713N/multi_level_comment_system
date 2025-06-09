be8e7cf6e98d5c41e2e86c8881c5faa840b441d5
PlutoA713N <mallachandu99@gmail.com>
2025-06-09 21:44:23 +0530

    feat: add expandCommentReplies() mongo query

--- src/models/mongo_aggregation_pipelines/index.ts
+++ src/models/mongo_aggregation_pipelines/index.ts
@@ -8,6 +8,11 @@ export const matchTopLevelComments = (postId: number): PipelineStage => ({
     },
 });
 
+
+export const limitTopLevelComments = (limit: number) => ({
+    $limit: limit
+})
+
 // ✅ $sort stage
 export const sortParentComments = (field: string, order: 1 | -1): PipelineStage => ({
     $sort: {
@@ -53,4 +58,61 @@ export const projectFinalShape: PipelineStage = {
         replies: 1,
         totalReplies: 1,
     },
+};
+
+
+export const expandCommentReplies = (
+    postId: number,
+    commentId: number,
+    page: number,
+    pageSize: number
+): PipelineStage[] => {
+    const offset = (page - 1) * pageSize;
+
+    return [
+        {
+            $match: {
+                postId,
+                commentId,
+                // parentCommentId: null
+            }
+        },
+        {
+            $lookup: {
+                from: "comments",
+                let: { parentId: "$commentId" },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: { $eq: ["$parentCommentId", "$$parentId"] }
+                        }
+                    },
+                    { $sort: { createdAt: -1 } },
+                    { $skip: offset },
+                    { $limit: pageSize },
+                    {
+                        $project: {
+                            _id: 0,
+                            id: "$commentId",
+                            text: 1,
+                            createdAt: 1
+                        }
+                    }
+                ],
+                as: "replies"
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                id: "$commentId",
+                text: 1,
+                createdAt: 1,
+                postId: 1,
+                parentCommentId: 1,
+                replies: 1,
+                totalReplies: 1
+            }
+        }
+    ];
 };

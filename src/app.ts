2f988a0db3af081475a2cd94302479eb95dc4cdd
PlutoA713N <mallachandu99@gmail.com>
2025-06-11 13:59:06 +0530

    feat: add architecture and not-found paths

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import httpLogger from "./middleware/logger_middleware/http.logger";
@@ -7,7 +8,8 @@ import {errorHandler} from "./middleware/express_middleware/error.middleware";
 import {helmetOptions} from "./helmet/helmet.options";
 import {apiLimiter} from "./middleware/express_middleware/rate.limiter.middleware";
 import {requestIdMiddleware} from "./middleware/express_middleware/requestId.middleware";
-import {notFoundMiddleware} from "./middleware/express_middleware/not.found.middleware";
+import path from "node:path";
+import HTTP from "http-status-codes";
 
 import healthRoutes from "./routes/health.routes";
 import userRoutes from "./routes/user/index"
@@ -23,15 +25,23 @@ app.use(httpLogger)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 setupSwaggerDocs(app)
+
+app.get('/', (req: Request, res: Response) => {
+   res.redirect("/Architecture.html")
+});
 
 app.use("/health", healthRoutes)
 app.use("/user", userRoutes)
 app.use("/api/posts", postRoutes)
 
 
-app.use(notFoundMiddleware)
+app.use((req: Request, res: Response) => {
+    res.status(HTTP.NOT_FOUND).send(path.join(__dirname, "../public/Not-Found.html"));
+})
+
 app.use(errorHandler)
 
 export {app}
